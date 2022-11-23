618e80803c821fb4e414963750d5304c36bbcdfb
Fathur Walkers <fathurwalkers44@gmail.com>
2022-11-23 03:24:40 -0800

    update

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -81,13 +81,21 @@ class GenerateController extends Controller
     {
         $faker                  = Faker::create('id_ID');
 
-        $array_lat_long = [
-            ['-5.487045', '122.581907'],
-            ['-5.490170', '122.577541'],
-            ['-5.493811', '122.575308'],
-            ['-5.496801', '122.579934'],
-            ['-5.496855', '122.576307'],
+        $array_lat = [
+            '5.487045',
+            '5.490170',
+            '5.493811',
+            '5.496801',
+            '5.496855'
         ];
+        $array_long = [
+            '122.581907',
+            '122.577541',
+            '122.575308',
+            '122.579934',
+            '122.576307'
+        ];
+
         $array_tempat = [
             'Kelurahan Lipu',
             'Keraton',
@@ -101,7 +109,7 @@ class GenerateController extends Controller
             "Acara Adat",
             "Acara Umum",
             "Agenda Umum",
-            "Acara Pernikahan",
+            "Acara Pernikahan"
         ];
 
         $bulan = Bulan::all();
@@ -119,8 +127,8 @@ class GenerateController extends Controller
                 "agenda_nama" => $array_nama_agenda[$iter++],
                 "agenda_tempat" => $tempat,
                 "agenda_keterangan" => $faker->paragraph(4),
-                "agenda_lat" => $array_lat_long[$iter++][0],
-                "agenda_long" => $array_lat_long[$iter++][1],
+                "agenda_lat" => $array_lat[$iter++],
+                "agenda_long" => $array_long[$iter++],
                 "agenda_status" => "",
                 "agenda_penyelenggara" => $random_status,
                 "agenda_waktu" => now(),
@@ -128,6 +136,7 @@ class GenerateController extends Controller
                 "updated_at" => now()
             ]);
             $save_agenda->save();
+            // dd($save_agenda);
         }
         $agenda_all = Agenda::all();
         dd($agenda_all);
