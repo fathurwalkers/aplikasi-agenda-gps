107e35b2e094f5be17e291a860992e0c77e29a2c
Fathur Walkers <fathurwalkers44@gmail.com>
2023-01-12 00:01:26 -0800

    update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -77,25 +77,6 @@
             </div>
         </div>
     </div>
-    {{-- <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card h-100">
-            <div class="card-body">
-                <div class="row align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-uppercase mb-1">Total </div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">28</div>
-                        <div class="mt-2 mb-0 text-muted text-xs">
-                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                            <span>Since last month</span>
-                        </div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-calendar fa-2x text-primary"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
 </div>
 
 {{-- <div class="card mb-3">
