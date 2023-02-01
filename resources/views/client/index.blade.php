dc1fb8bd2ade7611677e981c9ce66c8e49b36984
Fathur Walkers <fathurwalkers44@gmail.com>
2023-02-01 09:13:15 -0800

    update

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.client-layout')
 
 @push('css')
-    <style>
-        /* .container {
-                        height: 800px!important;
-                    } */
-    </style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<style>
+    .item img {
+        width:100%!important;
+    }
+
+    .carousel-inner > .item > img {
+        height: 300px!important;
+        width: 250px!important;
+    }
+
+    .carousel-item img {
+        object-fit: cover!important;
+        object-position: center!important;
+        height: 40vh!important;
+        overflow: hidden!important;
+    }
+
+</style>
+
 @endpush
 
 @section('tombol-keluar')
@@ -23,19 +39,30 @@
     </div>
     <div class="row row-cols-1  justify-content-center">
 
-        <div class="row">
+        <div class="row mb-3">
+            <div class="col-sm-12 col-md-12 col-lg-12 text-center">
+                <span class="selamatdatang">
+                    <b>
+                        Selamat Datang
+                    </b>
+                </span>
+            </div>
+        </div>
+
+        <div class="row mb-4">
             <div class="col-sm-12 col-md-12 col-lg-12">
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="..." alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="..." alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="..." alt="Third slide">
-                        </div>
+
+                        @for ($i = 1; $i < 5; $i++)
+                            <div class="carousel-item @if($i == 1) active @endif">
+                                @php
+                                    $slide = 'assets/slide' . $i . '.jpeg';
+                                @endphp
+                                <img class="img img-responsive" width="100%" height="60%" src="{{ asset($slide) }}" alt="First slide">
+                            </div>
+                        @endfor
+
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -48,6 +75,8 @@
                 </div>
             </div>
         </div>
+
+        <br />
 
         <div class="col-10 mb-4 btn shadow ">
             <a href="{{ route('client-daftar-agenda') }}">
@@ -107,3 +136,9 @@
 
     </div>
 @endsection
+
+@push('js')
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+@endpush
